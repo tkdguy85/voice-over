f8f876f2812d8c6e2e9deff53103daf6c4d476f9
Dallas Gordon III <dallas@wearefine.com>
2023-04-10 19:08:48 -0700

    Config reset

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `@import "@/styles/_variables.scss";` // Replace with the path to your SCSS variables file
-      }
-    }
-  }
+  transpileDependencies: true
 })
