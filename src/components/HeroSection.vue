250d0ca10f5893bbb79066659b3a6ea315386af6
Dallas Gordon III <dallas@wearefine.com>
2025-06-18 16:46:05 -0700

    Carousel set up

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,14 +1,51 @@
 <template>
   <div class="hero-container">
     <div class="link">
-      <div class="portrait-container">
-        <img class="portrait" src="../assets/studio-shot.jpg" alt="Self portrait in studio">
-      </div>
 
+      <v-carousel height="400px" hide-delimiters>
+        <v-carousel-item 
+          v-for="(photo, index) in carouselPhotos" :key="index">
+          <div class="portrait-container">
+            <img class="portrait" :src="photo.src" :alt="photo.alt">
+          </div>
+        </v-carousel-item>
+      </v-carousel>
+      
       <p>Work with me and let my voices bring some extra life to your projects! Let's go!</p>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      carouselPhotos: [
+        { 
+          src: require('../assets/carousel/studio-shot.jpg'), 
+          alt: 'Self portrait in studio' 
+        },
+        { 
+          src: require('../assets/carousel/in-the-studio.jpg'), 
+          alt: 'Salena in a studio booth' 
+        },
+        { 
+          src: require('../assets/carousel/model-shot.jpg'), 
+          alt: 'Model pose in black and white' 
+        },
+        { 
+          src: require('../assets/carousel/headshot-side.png'), 
+          alt: 'Salena\'s headshot' 
+        },
+        { 
+          src: require('../assets/carousel/studio-pro.jpg'), 
+          alt: 'Salena smiling in a studio booth' 
+        }
+      ]
+    };
+  }
+}
+</script>
 
 <style lang="scss">
   .hero-container {
