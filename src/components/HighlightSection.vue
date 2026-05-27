9d24a51a1fd8ffcf8e11e6779fd19c5ac358504c
Dallas3 <dallasgordon3@gmail.com>
2026-05-26 18:52:53 -0700

    Updated highlights cards

--- src/components/HighlightSection.vue
+++ src/components/HighlightSection.vue
@@ -3,10 +3,12 @@
     <h1>Highlights</h1>
     <div class="highlights-container">
       <div class="highlight-item" v-for="(item, index) in highlights" :key="index">
-        <img class="highlight-image" :src="item.image" :alt="item.alt">
+        <div class="highlight-image_wrapper">
+          <img class="highlight-image" :src="item.image" :alt="item.alt">
+        </div>
         <div class="highlight-content">
           <p class="highlight-title">{{ item.title }}</p>
-          <a class="highlight-link" :href="item.link" v-if="item.link">Read More</a>
+          <a class="highlight-link" :href="item.link" v-if="item.link" target="_blank">Read More Here</a>
         </div>
       </div>
     </div>
@@ -43,16 +45,17 @@ export default {
 <style lang="scss">
   .highlights-container {
     display: grid;
-    grid-template-columns: repeat(1fr);
     width: 100%;
     gap: 30px;
     text-align: center;
 
     @media (min-width: 1100px) {
+      gap: 40px;
       grid-template-columns: repeat(2, 1fr);
     }
 
     @media (min-width: 1300px) {
+      gap: 50px;
       grid-template-columns: repeat(3, 1fr);
     }
 
@@ -60,25 +63,59 @@ export default {
       display: flex;
       flex-direction: column;
       align-items: center;
-      justify-content: space-between;
-      padding: 30px 50px;
-      border: 1px solid #fff;
-      box-shadow: 15px 15px 10px #000;
-
-      img {
-        max-width: 300px;
-      }
+      overflow: hidden;
+      margin-top: 50px;
+      height: 100%;
 
       @media (min-width: 1100px) {
-        min-height: 200px;
-        padding: 30px;
+        justify-content: space-between;
+        margin-top: 30px;
       }
       
       @media (min-width: 1300px) {
-        padding: 20px;
-        min-height: 300px;
+        margin-top: 0;
+      }
+
+      .highlight-image_wrapper {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+        max-width: 300px;
+        object-position: center;
+        flex: 1;
+
+
+        @media (min-width: 1100px) {
+          max-width: 400px;
+        }
+        
+        @media (min-width: 1300px) {
+          max-width: 500px;
+        }
+
+
+        img {
+          width: 100%;
+          object-fit: cover;
+        }
       }
       
+      
+      .highlight-content {
+        margin-top: auto;
+      }
+      
+      .highlight-link {
+        text-decoration: none;
+        color: lightskyblue;
+        transition: transform 0.3s ease;
+        
+        &:hover {
+          color: white;
+        }
+      }   
     }
   }
 </style>
